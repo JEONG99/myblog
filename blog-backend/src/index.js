b87b6c06a40c70fdbd889cd968375a143e7d0d56
JEONG99 <jbh990621@gmail.com>
2022-07-28 22:22:19 +0900

    aaa

---
+++ blog-backend/src/index.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import api from './api/index.js';
+import jwtMiddleware from './lib/jwtMiddleware.js';
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+app.use(bodyParser());
+app.use(jwtMiddleware);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const corsOptions = {
+  origin: 'http://localhost:4000', // 허락하고자 하는 요청 주소
+  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+const port = PORT || 4000;
+app.listen(4000, () => {
+  console.log('Listening to port %d', port);
+});
+
+app.use(cors(corsOptions));
